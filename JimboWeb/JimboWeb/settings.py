f46613ef331c351b27d91d5b4510a67c7a2f0eab
Zhaobo-Wang <wangz393@mcmaster.ca>
2022-11-09 21:57:47 -0500

    frontend project page

--- JimboWeb/JimboWeb/settings.py
+++ JimboWeb/JimboWeb/settings.py
@@ -55,7 +55,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 4,
+    'PAGE_SIZE': 6,
 }
 
 MIDDLEWARE = [
